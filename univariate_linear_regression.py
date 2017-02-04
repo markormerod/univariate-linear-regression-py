8e091667c358185141b020705390dec5b2f93028
Mark Ormerod <markrormerod@gmail.com>
2017-02-04 00:34:20 +0000

    Adding prediction mode

--- univariate_linear_regression.py
+++ univariate_linear_regression.py
@@ -50,10 +50,10 @@ class UnivarLinearRegression(object):
             result1 = 0
             for val in self.normalized:
                 result0 += (theta0 + theta1 * val[0]) - val[1]
-                result0 = theta0 - alpha * (1/float(len(self.normalized))) * result0
+                result0 = theta0 - alpha * (1/(2*float(len(self.normalized)))) * result0
             for val in self.normalized:
                 result1 += ((theta0 + theta1 * val[0]) - val[1]) * val[0]
-                result1 = theta1 - alpha * (1/float(len(self.normalized))) * result1
+                result1 = theta1 - alpha * (1/(2*float(len(self.normalized)))) * result1
 
             runs -= 1
             theta0 = result0
@@ -62,7 +62,19 @@ class UnivarLinearRegression(object):
         result = self.runhypothesis(theta0, theta1, True)
         if not math.isnan(result):
             if self.best is None or self.best[0] > result:
-                self.best = [result, alpha]
+                self.best = [result, alpha, theta0, theta1]
+
+    def predict(self):
+        print("Entering prediction mode. To continue function generation, type \"guess\": ")
+        while True:
+            raw_x = raw_input("Enter an input value predict output using the best predictor function: ")
+            if raw_x in ["quit", "q"]:
+                sys.exit()
+            elif raw_x in ["guess", "g"]:
+                self.guess_loop()
+            x = float(raw_x)
+            y = self.best[2] + self.best[3] * x
+            print(str(x) + " -> " + str(y))
 
     def normalize_data(self):
         self.normalized = []
@@ -79,7 +91,7 @@ class UnivarLinearRegression(object):
 
         for idx, val in enumerate(self.data):
             self.normalized.append([(val[0] - min_x) / diff_x, (val[1] - min_y) / diff_y])
-            print(str(self.normalized[idx][0]) + "\t" + str(self.normalized[idx][1]))
+            # print(str(self.normalized[idx][0]) + "\t" + str(self.normalized[idx][1]))
     def printdata(self):
         print(', '.join(self.header))
         for row in self.data:
@@ -91,19 +103,28 @@ class UnivarLinearRegression(object):
         print("Follow prompts to find the best prediction function over values in " + sys.argv[1] + ". Type \"quit\" at any point to quit.")
 
         while True:
-            if self.best is not None:
+            hasbest = self.best is not None
+            if hasbest:
                 print("Best guess for learning rate so far: " + str(self.best[1]))
+                print("Type \"done\" at any point to use the best guess to make predictions.")
             try:
                 raw_alpha = raw_input("\nα (learning rate): ")
                 if raw_alpha in ["quit", "q"]:
                     sys.exit()
+                elif hasbest and raw_alpha in ["done", "d"]:
+                    self.predict()
                 alpha = float(raw_alpha)
 
-                raw_n = raw_input("Amount of learning iterations: ")
+                raw_n = raw_input("Amount of learning iterations (default 400): ")
                 if raw_n in ["quit", "q"]:
                     print('')
                     sys.quit()
-                n = int(raw_n)
+                elif hasbest and raw_alpha in ["done", "d"]:
+                    self.predict()
+                if raw_n == "":
+                    n = 400
+                else:
+                    n = int(raw_n)
 
                 univar.run_gradient_descent(n, alpha)
             except EOFError:
@@ -111,12 +132,13 @@ class UnivarLinearRegression(object):
                 sys.exit()
             except ValueError:
                 print('Please enter a number or type \"quit\" to exit')
-
+            except OverflowError:
+                print('Please choose smaller numbers')
 
 if (len(sys.argv)) < 2:
     print("No file path given for csv data file")
     sys.exit()
 
 univar = UnivarLinearRegression(sys.argv[1])
-
+univar.printdata()
 univar.guess_loop()
